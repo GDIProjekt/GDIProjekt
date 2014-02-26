f358f68598389df3dbafc66f743fc38481dbe57d
thonghuynh18 <thong.huynh@hotmail.de>
2014-02-26 21:23:29 +0100

    ENIGMA FUNKTIONIERT OMG I DIS REAL LYFE

--- src/de/tudarmstadt/gdi1/project/cipher/enigma/EnigmaImpl.java
+++ src/de/tudarmstadt/gdi1/project/cipher/enigma/EnigmaImpl.java
@@ -2,7 +2,7 @@ package src.de.tudarmstadt.gdi1.project.cipher.enigma;
 import java.util.List;
 
 import de.tudarmstadt.gdi1.project.cipher.enigma.Enigma;
-import java.util.ArrayList;
+
 
 public class EnigmaImpl implements Enigma{
 	List<Rotor> rotors;
@@ -39,11 +39,11 @@ public class EnigmaImpl implements Enigma{
 			
 			transChars[i]=pinboard.translate(transChars[i]);
 			
-				for (int j = 1; rotors.get(j-1).rotate() ; j++){
+				/**for (int j = 1; rotors.get(j-1).rotate() ; j++){
 						rotors.get(j).rotate();					
-				}
+				}**/
 				
-			rotors.get(0).rotate();
+			RotationAllRotors(0);
 			result += transChars[i];	
 			i++;
 			
@@ -53,12 +53,48 @@ public class EnigmaImpl implements Enigma{
 		return result;
 	}
 	
-	public void RotationAllRotors(){
-		int i = 0;
+	public void RotationAllRotors(int i){
+	
+		if((rotors.get(i).rotate())&&(i<rotors.size()))
+			RotationAllRotors(i++);
 	}
 	
 	
 	public String decrypt(String text){
-		return null;
+		char[] chars = text.toCharArray();
+		char[] transChars = new char[chars.length];
+		String result = "";
+		int i = 0;
+		//Einzelne Buchstaben durch Pinboard und durch alle Rotoren
+		for(Character c: chars){
+			transChars[i] = pinboard.translate(c);
+			
+			//Buchstaben durch alle Rotoren prügeln
+			for(int j = 0; j < rotors.size(); j++){				
+				transChars[i] = rotors.get(j).translate(transChars[i], true);				
+			}
+			
+			//Buchstaben durch reverseRotor werfen
+			transChars[i] = reverseRotor.translate(transChars[i]);
+			
+			//Buchstaben durch alle Rotoren in umgekehrter Reihenfolge kloppen
+			for(int k = rotors.size()-1; k >= 0; k--){				
+				transChars[i] = rotors.get(k).translate(transChars[i], false);			
+			}
+			
+			transChars[i]=pinboard.translate(transChars[i]);
+			
+				/**for (int j = 1; rotors.get(j-1).rotate() ; j++){
+						rotors.get(j).rotate();					
+				}**/
+				
+			rotors.get(0).rotate();
+			result += transChars[i];	
+			i++;
+			
+		}
+		for (int z = 0; z < rotors.size(); z++) rotors.get(z).reset();
+		
+		return result;
 	}
 }
